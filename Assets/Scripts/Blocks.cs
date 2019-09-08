fb8a0db3e8636f9f221b5f386aee8c44e535ac6b
416C <picko934@gmail.com>
2019-09-08 16:23:15 +1000

    Update blocks motions

---
+++ Assets/Scripts/Blocks.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Left Key Pressed");
+            //modify position
+            transform.position += new Vector3(-1, 0, 0);
+
+            if (isValidPos())
+            {
+                updateGrid();
+            }
+            else
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Right Key Pressed");
+            //modify position
+            transform.position += new Vector3(1, 0, 0);
+
+            if (isValidPos())
+            {
+                updateGrid();
+            }
+            else
+            {
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Key Pressed");
+            //modify position
+            transform.Rotate(0, 0, -90);
+
+            if (isValidPos())
+            {
+                updateGrid();
+            }
+            else
+            {
+                transform.Rotate(0, 0, 90);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0);
+
+            if (isValidPos())
+            {
+                updateGrid();
+            }
+            else
+            {
+                transform.position += new Vector3(0, 1, 0);
+                Playfield.deleteAllRows();
+                FindObjectOfType<Spawner>().spawnerNext();
+                enabled = false;
+            }
+        }
+        
+    }
+
+    bool isValidPos()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Playfield.roundVec2(child.position);
+
+            //not inside border
+            if (!Playfield.inBorder(child.position))
+            {
+                return false;
+            }
+
+            //block in grid cell
+            if (Playfield.grid[(int)v.x, (int)v.y] != null && Playfield.grid[(int)v.x, (int)v.y].parent != transform)
+            {
+                return false;
+            }
+            
+        }
+
+        return true;
+    }
+
+    void updateGrid()
+    {
+        //remove oldchildren from grid
+        for (int y = 0; y < Playfield.height; y++)
+        {
+            for (int x = 0; x < Playfield.width; x++)
+            {
+                if(Playfield.grid[x,y] != null)
+                {
+                    if (Playfield.grid[x, y].parent == transform)
+                    {
+                        Playfield.grid[x, y] = null;
+                    }
+                }
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            Vector2 v = Playfield.roundVec2(child.position);
+            Playfield.grid[(int)v.x, (int)v.y] = child;
+        }
+    }
+
+}
