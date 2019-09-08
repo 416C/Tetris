28596d004392da4edafc6ef83b53ff20dfe75f89
416C <picko934@gmail.com>
2019-09-08 16:35:34 +1000

    Update Blocks.cs

--- Assets/Scripts/Blocks.cs
+++ Assets/Scripts/Blocks.cs
@@ -4,139 +4,134 @@ using UnityEngine;
 
 public class Blocks : MonoBehaviour
 {
-<<<<<<< HEAD
     // Start is called before the first frame update
     void Start()
     {
-        
-=======
-    float lastfall = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        if (!isValidPos())
-        {
-            Debug.Log("GameOver");
-            Destroy(gameObject);
-        }
->>>>>>> features
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-<<<<<<< HEAD
-=======
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        float lastfall = 0;
+        // Start is called before the first frame update
+        void Start()
         {
-            Debug.Log("Left Key Pressed");
-            //modify position
-            transform.position += new Vector3(-1, 0, 0);
-
-            if (isValidPos())
+            if (!isValidPos())
             {
-                updateGrid();
-            }
-            else
-            {
-                transform.position += new Vector3(1, 0, 0);
+                Debug.Log("GameOver");
+                Destroy(gameObject);
             }
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Debug.Log("Right Key Pressed");
-            //modify position
-            transform.position += new Vector3(1, 0, 0);
 
-            if (isValidPos())
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                updateGrid();
-            }
-            else
-            {
+                Debug.Log("Left Key Pressed");
+                //modify position
                 transform.position += new Vector3(-1, 0, 0);
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Debug.Log("Up Key Pressed");
-            //modify position
-            transform.Rotate(0, 0, -90);
 
-            if (isValidPos())
-            {
-                updateGrid();
-            }
-            else
-            {
-                transform.Rotate(0, 0, 90);
-            }
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - lastfall >= 1)
-        {
-            transform.position += new Vector3(0, -1, 0);
-
-            if (isValidPos())
-            {
-                updateGrid();
-            }
-            else
-            {
-                transform.position += new Vector3(0, 1, 0);
-                Playfield.deleteAllRows();
-                FindObjectOfType<Spawner>().spawnerNext();
-                enabled = false;
-            }
-            lastfall = Time.time;
-        }
->>>>>>> features
-        
-    }
-
-    bool isValidPos()
-    {
-        foreach (Transform child in transform)
-        {
-            Vector2 v = Playfield.roundVec2(child.position);
-
-            //not inside border
-            if (!Playfield.inBorder(child.position))
-            {
-                return false;
-            }
-
-            //block in grid cell
-            if (Playfield.grid[(int)v.x, (int)v.y] != null && Playfield.grid[(int)v.x, (int)v.y].parent != transform)
-            {
-                return false;
-            }
-            
-        }
-
-        return true;
-    }
-
-    void updateGrid()
-    {
-        //remove oldchildren from grid
-        for (int y = 0; y < Playfield.height; y++)
-        {
-            for (int x = 0; x < Playfield.width; x++)
-            {
-                if(Playfield.grid[x,y] != null)
+                if (isValidPos())
                 {
-                    if (Playfield.grid[x, y].parent == transform)
+                    updateGrid();
+                }
+                else
+                {
+                    transform.position += new Vector3(1, 0, 0);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                Debug.Log("Right Key Pressed");
+                //modify position
+                transform.position += new Vector3(1, 0, 0);
+
+                if (isValidPos())
+                {
+                    updateGrid();
+                }
+                else
+                {
+                    transform.position += new Vector3(-1, 0, 0);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Debug.Log("Up Key Pressed");
+                //modify position
+                transform.Rotate(0, 0, -90);
+
+                if (isValidPos())
+                {
+                    updateGrid();
+                }
+                else
+                {
+                    transform.Rotate(0, 0, 90);
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) || Time.time - lastfall >= 1)
+            {
+                transform.position += new Vector3(0, -1, 0);
+
+                if (isValidPos())
+                {
+                    updateGrid();
+                }
+                else
+                {
+                    transform.position += new Vector3(0, 1, 0);
+                    Playfield.deleteAllRows();
+                    FindObjectOfType<Spawner>().spawnerNext();
+                    enabled = false;
+                }
+                lastfall = Time.time;
+            }
+
+        }
+
+        bool isValidPos()
+        {
+            foreach (Transform child in transform)
+            {
+                Vector2 v = Playfield.roundVec2(child.position);
+
+                //not inside border
+                if (!Playfield.inBorder(child.position))
+                {
+                    return false;
+                }
+
+                //block in grid cell
+                if (Playfield.grid[(int)v.x, (int)v.y] != null && Playfield.grid[(int)v.x, (int)v.y].parent != transform)
+                {
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+
+        void updateGrid()
+        {
+            //remove oldchildren from grid
+            for (int y = 0; y < Playfield.height; y++)
+            {
+                for (int x = 0; x < Playfield.width; x++)
+                {
+                    if (Playfield.grid[x, y] != null)
                     {
-                        Playfield.grid[x, y] = null;
+                        if (Playfield.grid[x, y].parent == transform)
+                        {
+                            Playfield.grid[x, y] = null;
+                        }
                     }
                 }
             }
-        }
 
-        foreach (Transform child in transform)
-        {
-            Vector2 v = Playfield.roundVec2(child.position);
-            Playfield.grid[(int)v.x, (int)v.y] = child;
+            foreach (Transform child in transform)
+            {
+                Vector2 v = Playfield.roundVec2(child.position);
+                Playfield.grid[(int)v.x, (int)v.y] = child;
+            }
         }
     }
 
